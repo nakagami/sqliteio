05c715ebe717b2be2102f6e6401969d5206a5695
Hajime Nakagami <nakagami@gmail.com>
2024-06-16 19:00:28 +0900

    update copyright year

--- sqliteio/__init__.py
+++ sqliteio/__init__.py
@@ -1,7 +1,7 @@
 ################################################################################
 # MIT License
 #
-# Copyright (c) 2023 Hajime Nakagami<nakagami@gmail.com>
+# Copyright (c) 2023, 2024 Hajime Nakagami<nakagami@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
